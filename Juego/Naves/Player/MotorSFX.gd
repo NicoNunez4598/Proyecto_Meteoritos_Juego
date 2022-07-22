5136743d679f58eeccf4efedee95ad63b56d209c
NicoNunez4598 <88166508+NicoNunez4598@users.noreply.github.com>
2022-07-22 20:59:59 -0300

    Estela y Motor
    Agregado:
    - Estela (VFX) + Input
    - Motor (SFX) + Input

---
+++ Juego/Naves/Player/MotorSFX.gd
@@ -0,0 +1,40 @@
+#Motor.gd
+class_name Motor
+extends AudioStreamPlayer2D
+
+## Atributos Export
+export var tiempo_transicion:float = 0.6
+export var volumen_apagado:float = -30.0
+
+## Atributos Onready
+onready var tween_sonido:Tween = $Tween
+
+## Atributos
+var volumen_original:float
+
+## Metodos
+func _ready() -> void:
+	volumen_original = volume_db
+	volume_db = volumen_apagado
+
+## Metodos Custom
+func sonido_on() -> void:
+	if not playing:
+		play()
+	
+	efecto_transicion(volume_db, volumen_original)
+
+func sonido_off() -> void:
+	efecto_transicion(volume_db, volumen_apagado)
+
+func efecto_transicion(desde_vol:float, hasta_vol:float) -> void:
+	tween_sonido.interpolate_property(
+		self,
+		"volume_db",
+		desde_vol,
+		hasta_vol,
+		tiempo_transicion,
+		Tween.TRANS_LINEAR,
+		Tween.EASE_OUT_IN
+	)
+	tween_sonido.start()
