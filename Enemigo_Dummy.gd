2ca2af3c3d5fd8612f89c9216469891d202908d8
NicoNunez4598 <88166508+NicoNunez4598@users.noreply.github.com>
2022-07-26 10:31:04 -0300

    Disparos
    Agregado
    - Daño + Destruccion al Enemigo Dummy con ambas armas
    - Daño al player y sonido de impacto

--- Enemigo_Dummy.gd
+++ Enemigo_Dummy.gd
@@ -1,6 +1,19 @@
 #Enemigo_Dummy.gd
 extends Node2D
 
-func _on_Area2D_body_entered(body: Node) -> void:
+## Atributos
+var hitpoints:float = 10.0
+
+## Metodos
+func recibir_danio(danio: float) -> void:
+	hitpoints -= danio
+	if hitpoints <= 0.0:
+		queue_free()
+
+func _process(delta: float) -> void:
+	$Canion.set_esta_disparando(true)
+
+## Señales
+func _on_body_entered(body: Node) -> void:
 	if body is Player:
 		body.destruir()
